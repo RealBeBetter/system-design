23e9e3c9fa7b25f0848231e68b88fc4313d237c3
Real <swrely@qq.com>
2022-11-14 00:07:26 +0800

    feat: init anti-brushing and redis-mq

---
+++ anti-brushing/src/main/java/com/company/antibrushing/AntiBrushingApplication.java
@@ -0,0 +1,13 @@
+package com.company.antibrushing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiBrushingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntiBrushingApplication.class, args);
+    }
+
+}
---
+++ anti-brushing/src/test/java/com/company/antibrushing/AntiBrushingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.antibrushing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiBrushingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
