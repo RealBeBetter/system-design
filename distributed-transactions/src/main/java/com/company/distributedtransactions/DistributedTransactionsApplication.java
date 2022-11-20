80eda3663c0dc656a56d5f4a1629dcb42c1d4fab
Real <swrely@qq.com>
2022-11-20 21:49:19 +0800

    feat: init distributed transaction

---
+++ distributed-transactions/src/main/java/com/company/distributedtransactions/DistributedTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.company.distributedtransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedTransactionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedTransactionsApplication.class, args);
+    }
+
+}
