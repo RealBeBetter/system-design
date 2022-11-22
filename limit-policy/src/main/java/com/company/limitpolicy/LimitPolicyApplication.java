e55e366b9d09d649eab6ae193f08a1d49103b1c3
Real <swrely@qq.com>
2022-11-22 23:45:40 +0800

    feat: add limit policy

---
+++ limit-policy/src/main/java/com/company/limitpolicy/LimitPolicyApplication.java
@@ -0,0 +1,13 @@
+package com.company.limitpolicy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitPolicyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimitPolicyApplication.class, args);
+    }
+
+}
---
+++ limit-policy/src/test/java/com/company/limitpolicy/LimitPolicyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.limitpolicy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitPolicyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
